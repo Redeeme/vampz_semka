3068479849428c0dbc7f5e7603310e70e5fd8cc3
Redeeme <Spetsnas17@gmail.com>
2022-04-09 21:19:52 +0200

    cart databindings

--- app/src/main/java/com/example/myapplication/cart/CartAdapter.kt
+++ app/src/main/java/com/example/myapplication/cart/CartAdapter.kt
@@ -1,62 +1,53 @@
 package com.example.myapplication.cart
 
-import android.annotation.SuppressLint
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.IProductClickListener
-import com.example.myapplication.R
+import com.example.myapplication.databinding.ItemCartLayoutBinding
 import com.example.myapplication.product.ProductModelClass
 
-class CartAdapter (private val itemClickListener: IProductClickListener) :
+class CartAdapter(private val itemClickListener: IProductClickListener) :
     RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
     private var list: ArrayList<ProductModelClass> = ArrayList()
 
+    class ViewHolder(val binding: ItemCartLayoutBinding) : RecyclerView.ViewHolder(binding.root) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        fun bind(item: ProductModelClass) {
+            binding.productItem = item
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.item_cart_layout, parent, false)
 
-        return ViewHolder(view)
+        val listItemBinding = ItemCartLayoutBinding.inflate(view, parent, false)
+
+        return ViewHolder(listItemBinding)
     }
-    @SuppressLint("SetTextI18n")
+
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        (holder as ViewHolder).bind(list[position])
 
-        val itemsViewModel = list[position]
-        (holder as ViewHolder).tvProductName.text = itemsViewModel.productName
-        holder.tvProductOrigin.text = itemsViewModel.productOrigin
-        holder.ivProductImage.setImageResource(itemsViewModel.productImage)
-        holder.tvProductPrice.text = itemsViewModel.productPrice.toString() + " €/kg"
-        holder.tvProductAmount.text = itemsViewModel.productAmount.toString()
-        holder.ivSub.setOnClickListener{
-            itemClickListener.minus(list[position],position)
+        holder.binding.sub.setOnClickListener {
+            itemClickListener.minus(list[position], position)
         }
-        holder.ivAdd.setOnClickListener{
-            itemClickListener.add(list[position],position)
+        holder.binding.add.setOnClickListener {
+            itemClickListener.add(list[position], position)
         }
-        holder.ivDeleteFromCart.setOnClickListener{
-            itemClickListener.cartButton(list[position],position)
+        holder.binding.ivDeleteFromCart.setOnClickListener {
+            itemClickListener.cartButton(list[position], position)
         }
     }
+
     override fun getItemCount(): Int {
         return list.size
     }
-    class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
-        val tvProductName: TextView = itemView.findViewById(R.id.id_productName)
-        val tvProductOrigin: TextView = itemView.findViewById(R.id.id_productOrigin)
-        val ivProductImage: ImageView = itemView.findViewById(R.id.id_productImage)
-        val tvProductPrice: TextView = itemView.findViewById(R.id.id_productPrice)
-        val tvProductAmount: TextView = itemView.findViewById(R.id.id_productAmount)
-        val ivAdd: ImageView = itemView.findViewById(R.id.add)
-        val ivSub: ImageView = itemView.findViewById(R.id.sub)
-        val ivDeleteFromCart: ImageView = itemView.findViewById(R.id.iv_delete_from_cart)
-    }
-    fun setData(newList: ArrayList<ProductModelClass>){
-        val diffUtil = CartDiffUtil(list,newList)
+
+    fun setData(newList: ArrayList<ProductModelClass>) {
+        val diffUtil = CartDiffUtil(list, newList)
         val diffResult = DiffUtil.calculateDiff(diffUtil)
         list = newList
         diffResult.dispatchUpdatesTo(this)
--- app/src/main/java/com/example/myapplication/shop/ShopAdapter.kt
+++ app/src/main/java/com/example/myapplication/shop/ShopAdapter.kt
@@ -1,6 +1,5 @@
 package com.example.myapplication.shop
 
-import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
@@ -11,21 +10,14 @@ import com.example.myapplication.product.ProductModelClass
 
 class ShopAdapter(private val itemClickListener: IProductClickListener) :
     RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
     private var list: List<ProductModelClass> = emptyList()
+
     class ViewHolder(val binding: ItemProductLayoutBinding) : RecyclerView.ViewHolder(binding.root) {
+
         fun bind(item: ProductModelClass){
             binding.productItem = item
         }
-
-        /*val tvProductName: TextView = itemView.findViewById(R.id.id_productName)
-        val tvProductOrigin: TextView = itemView.findViewById(R.id.id_productOrigin)
-        val ivProductImage: ImageView = itemView.findViewById(R.id.id_productImage)
-        val tvProductPrice: TextView = itemView.findViewById(R.id.id_productPrice)
-        val tvCount: TextView = itemView.findViewById(R.id.id_productAmount)
-        val ivAdd: ImageView = itemView.findViewById(R.id.add)
-        val ivSub: ImageView = itemView.findViewById(R.id.sub)
-        val ivAddToCart: ImageView = itemView.findViewById(R.id.iv_add_to_cart)
-        val tvAddToCart: TextView = itemView.findViewById(R.id.tv_add_to_cart)*/
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -37,17 +29,9 @@ class ShopAdapter(private val itemClickListener: IProductClickListener) :
     }
 
     //TODO data bindings
-    @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         (holder as ViewHolder).bind(list[position])
 
-        /*val itemsViewModel = list[position]
-        (holder as ViewHolder).tvProductName.text = itemsViewModel.productName
-        holder.tvProductOrigin.text = itemsViewModel.productOrigin
-        holder.ivProductImage.setImageResource(itemsViewModel.productImage)
-        holder.tvProductPrice.text = itemsViewModel.productPrice.toString() + " €/kg"
-        holder.tvCount.text = "${itemsViewModel.productAmount}"
-        */
         holder.binding.sub.setOnClickListener{
             itemClickListener.minus(list[position],position)
         }
@@ -60,9 +44,7 @@ class ShopAdapter(private val itemClickListener: IProductClickListener) :
         holder.binding.tvAddToCart.setOnClickListener {
             itemClickListener.cartButton(list[position],position)
         }
-        /*holder.ivProductImage.setOnClickListener {
-            itemClickListener.show(list[position])
-        }*/
+
     }
     override fun getItemCount(): Int {
         return list.size
